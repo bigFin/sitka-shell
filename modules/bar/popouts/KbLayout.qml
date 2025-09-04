26heij/caelestia-dots-kde

bar: add kblayout popout

modules/bar/popouts/KbLayout.qml
@@ -1,0 +1,6 @@
+import qs.components
+import qs.services
+
+StyledText {
+    text: qsTr("Keyboard layout: %1").arg(Hyprland.kbLayoutFull)
+}

bigFin/sitka-shell

modules/bar/popouts/KbLayout.qml
@@ -1,0 +1,6 @@
+import qs.components
+import qs.services
+
+StyledText {
+    text: qsTr("Keyboard layout: %1").arg(Niri.kbLayouts)
+}